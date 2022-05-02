8277289fec01e97707a0c78731f21c8cc664be37
18bce133L <18bce133@nirmauni.ac.in>
2022-05-02 10:56:21 +0530

    4. Inorder added to check order of println.

--- src/test/java/com/example/PrintStatement.java
+++ src/test/java/com/example/PrintStatement.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.verify;
 
@@ -21,9 +23,10 @@ public class PrintStatement {
         account.deposit(500);
         account.printStatement();
 
-        verify(print).println("DATE | AMOUNT | BALANCE\n");
-        verify(print).println("10/04/2014 | 500.00 | 1400.00\n");
-        verify(print).println("02/04/2014 | -100.00 | 900.00\n");
-        verify(print).println("01/04/2014 | 1000.00 | 1000.00\n");
+        InOrder inOrder= Mockito.inOrder(print);
+        inOrder.verify(print).println("DATE | AMOUNT | BALANCE\n");
+        inOrder.verify(print).println("10/04/2014 | 500.00 | 1400.00\n");
+        inOrder.verify(print).println("02/04/2014 | -100.00 | 900.00\n");
+        inOrder.verify(print).println("01/04/2014 | 1000.00 | 1000.00\n");
     }
 }
